5d64cddba7427c8593c3938b2d2c7eee1f2e88cd
schaef11 <48692595+schaef11@users.noreply.github.com>
2020-03-27 16:08:52 -0500

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,29 @@
+
+public class Animal 
+{
+	String name, diet, commonRegion, prefferedBiome, animalClass;
+	int numOfLimbs, averageLifespan; 
+	String[] methodsOfTravel = new String[10];
+	String[] comments = new String[500]; 
+	boolean coldOrWarmBlooded; // false == cold blooded
+	
+	String[] getTraits()
+	{
+		return null; 
+	}
+	
+	boolean addTrait()
+	{
+		return false;
+	}
+	
+	String[] getComments()
+	{
+		return null; 
+	}
+	
+	boolean addComment(String comment)
+	{
+		return false; 
+	}
+}
