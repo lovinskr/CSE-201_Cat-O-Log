c874a1cc3adf0101e2e0dc1d02749cfae209e288
stego135 <62773652+stego135@users.noreply.github.com>
2020-04-01 18:40:40 -0700

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -5,11 +5,64 @@ public class Animal
 	int numOfLimbs, averageLifespan; 
 	String[] methodsOfTravel = new String[10];
 	String[] comments = new String[500]; 
+	int lastComment = 0;
 	boolean coldOrWarmBlooded; // false == cold blooded
 	
+	public Animal() {
+		
+	}
+	
+	String getName() {
+		return name;
+	}
+	
+	String getDiet() {
+		return diet;
+	}
+	
+	String getCommonRegion() {
+		return commonRegion;
+	}
+	
+	String getPreferredBiome() {
+		return prefferedBiome;
+	}
+	
+	String getAnimalClass() {
+		return animalClass;
+	}
+	
+	int getNumOfLimbs() {
+		return numOfLimbs;
+	}
+	
+	int getAverageLifespan() {
+		return averageLifespan;
+	}
+	
+	boolean getColdOrWarmBlooded() {
+		return coldOrWarmBlooded;
+	}
+	
+	/*
+	 * Returns name, diet, commonRegion, preferredBiome, animalClass, 
+	 * numOfLimbs, averageLifespan, coldOrWarmBlooded
+	 */
 	String[] getTraits()
 	{
-		return null; 
+		String[] traits = new String[8];
+		traits[0] = name;
+		traits[1] = diet;
+		traits[2] = commonRegion;
+		traits[3] = prefferedBiome;
+		traits[4] = animalClass;
+		traits[5] = Integer.toString(numOfLimbs);
+		traits[6] = Integer.toString(averageLifespan);
+		String blood;
+		if (coldOrWarmBlooded) blood = "warm blooded";
+		else blood = "cold blooded";
+		traits[7] = blood;
+		return traits; 
 	}
 	
 	boolean addTrait()
@@ -19,11 +72,14 @@ public class Animal
 	
 	String[] getComments()
 	{
-		return null; 
+		return comments; 
 	}
 	
 	boolean addComment(String comment)
 	{
-		return false; 
+		if (lastComment == 499) return false;
+		comments[lastComment] = comment;
+		lastComment++;
+		return true; 
 	}
 }
--- Catalog.java
+++ Catalog.java
@@ -1,5 +1,16 @@
 
 public class Catalog 
 {
-
+	Animal[] animals;
+	int lastAnimal = 0;
+	String searchFor;
+	
+	public boolean addAnimal(Animal newAnimal) {
+		animals[lastAnimal] = newAnimal;
+		lastAnimal++;
+		return true;
+	}
+	public Animal[] getAnimals() {
+		return animals;
+	}
 }
