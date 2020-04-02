fab53db6ea817b1cb9e603676fdfe7e5a8357ff0
stego135 <62773652+stego135@users.noreply.github.com>
2020-04-02 12:52:45 -0700

    Add files via upload

--- Catalog.java
+++ Catalog.java
@@ -5,7 +5,15 @@ public class Catalog
 	int lastAnimal = 0;
 	String searchFor;
 	
+	public Catalog() {
+		animals = new Animal[500];
+	}
+	public Catalog(Animal[] animal, int index) {
+		animals = animal;
+		lastAnimal = index;
+	}
 	public boolean addAnimal(Animal newAnimal) {
+		if (lastAnimal == animals.length) return false;
 		animals[lastAnimal] = newAnimal;
 		lastAnimal++;
 		return true;
