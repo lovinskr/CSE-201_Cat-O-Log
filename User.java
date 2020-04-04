6641a37bc1fd3101b64cc467a89fb0648a61f7f9
schaef11 <48692595+schaef11@users.noreply.github.com>
2020-04-04 16:58:37 -0500

    Add files via upload

--- User.java
+++ User.java
@@ -2,10 +2,44 @@
 public class User 
 {
 	// there has to at least be 1 of each except email
-	String[] email = new String[2]; 
-	String[] phoneNumber = new String[2]; 
-	String lastName, firstName; 
+	private String[] email = new String[2]; 
+	private String[] phoneNumber = new String[2]; 
+	private String lastName, firstName; 
+	private boolean administrator = false; 
 	
+	public User()
+	{
+	}
+	
+	boolean setFName(String firstName)
+	{
+		this.firstName = firstName; 
+		return false; 
+	}
+	
+	boolean setLName(String lastName)
+	{
+		this.lastName = lastName; 
+		return false; 
+	}
+	
+	boolean setEmail(String[] email)
+	{
+		this.email = email; 
+		return true; 
+	}
+	
+	boolean setPhoneNumber(String[] phoneNumber)
+	{
+		this.phoneNumber = phoneNumber; 
+		return true; 
+	}
+	
+	boolean setAdmin(boolean admin)
+	{
+		administrator = admin; 
+		return true; 
+	}
 	boolean requestAnimal(Animal newAnimal)
 	{
 		return false; 
@@ -14,6 +48,11 @@ public class User
 	boolean loggedIn(String username, String password)
 	{
 		return false;
+	}
+	
+	boolean isAdmin()
+	{
+		return administrator ? true : false ; 
 	}
 	
 	void commentOnAnimal(String comment)
